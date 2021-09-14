ce4418a7a4808bad3c20ddab6e5c5c1338ad3441
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-09-14 06:48:13 -0500

    Update eventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 //El suffix indicates variables that hold DOM elements
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEL = document.querySelector("#tasks-to-do");
 
-//callback function *notice the lack of parentheses for createTaskHandler*
-buttonEl.addEventListener("click", crateTaskHandler);
+//Callback function *notice the lack of parentheses for createTaskHandler*
+formEl.addEventListener("submit", crateTaskHandler);
 
-function crateTaskHandler() {
+function crateTaskHandler(event) {
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "New Task";
