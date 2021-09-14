ab1edcb5e3118efc1d919a260e01a72b9f89c23a
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-09-14 08:27:07 -0500

    Refactor with taskDataObj (object)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,36 @@
-//El suffix indicates variables that hold DOM elements
+//El suffix indicates variables that hold DOM elements.
 var formEl = document.querySelector("#task-form");
 var tasksToDoEL = document.querySelector("#tasks-to-do");
 
-//Callback function *notice the lack of parentheses for createTaskHandler*
-formEl.addEventListener("submit", crateTaskHandler);
+//createTaskHandler is a callback function (function passed as an argument).
+formEl.addEventListener("submit", taskFormHandler);
 
-function crateTaskHandler(event) {
+function taskFormHandler(event) {
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form.");
+        return false;
+    }
     event.preventDefault();
+
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    }
+
+    createTaskEl(taskDataObj);
+    formEl.reset();
+}
+
+function createTaskEl(taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "New Task";
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
     tasksToDoEL.appendChild(listItemEl);
 }
